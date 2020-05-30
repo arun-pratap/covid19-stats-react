53731e450546753cd7d21f69399b225f5e9b58c4
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2020-05-30 17:30:44 +0530

    Updates

---
+++ precache-manifest.13d27521e1b3858cb385d4e0416cb847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fb575b68636eb4bb734e791527fb2c",
+    "url": "/covid19stats-react/index.html"
+  },
+  {
+    "revision": "4f834e7cc6d7544810b8",
+    "url": "/covid19stats-react/static/css/main.555671cc.chunk.css"
+  },
+  {
+    "revision": "42649a504443cd763b4f",
+    "url": "/covid19stats-react/static/js/2.8d805f6c.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19stats-react/static/js/2.8d805f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f834e7cc6d7544810b8",
+    "url": "/covid19stats-react/static/js/main.21c55512.chunk.js"
+  },
+  {
+    "revision": "2c51a12d318341b7177e",
+    "url": "/covid19stats-react/static/js/runtime-main.73c0d0a1.js"
+  }
+]);
