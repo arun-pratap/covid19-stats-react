49ca7e5e00aa195d297aecb76f8d04b8805d124d
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2020-05-30 19:08:48 +0530

    Updates

---
+++ precache-manifest.8039183bfc57eaf92d4bf75aa0ea40d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce74156331deee9d0550681e42df47d",
+    "url": "/covid19-stats-react/index.html"
+  },
+  {
+    "revision": "4f834e7cc6d7544810b8",
+    "url": "/covid19-stats-react/static/css/main.555671cc.chunk.css"
+  },
+  {
+    "revision": "42649a504443cd763b4f",
+    "url": "/covid19-stats-react/static/js/2.8d805f6c.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-stats-react/static/js/2.8d805f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f834e7cc6d7544810b8",
+    "url": "/covid19-stats-react/static/js/main.21c55512.chunk.js"
+  },
+  {
+    "revision": "8cb8b48fc27b39b7570c",
+    "url": "/covid19-stats-react/static/js/runtime-main.2bf2631c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19stats-react/precache-manifest.13d27521e1b3858cb385d4e0416cb847.js"
+  "/covid19-stats-react/precache-manifest.8039183bfc57eaf92d4bf75aa0ea40d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19stats-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-stats-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
